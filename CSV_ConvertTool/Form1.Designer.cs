c780945849a17d7f0eaaf1da5540fb0fdfe2535f
齊藤 勇介 <YusSaito@intra.wiseman.co.jp>
2019-03-26 11:08:37 +0900

    Initial Commit

---
+++ CSV_ConvertTool/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace CSV_ConvertTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(319, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "変換開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(641, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 31);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ファイルを選択";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(758, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 142);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CSVコンバートツール by業務支援課 齊藤";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ CSV_ConvertTool/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CSV_ConvertTool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //ファイルを選択するボタン
+            string select_file_path = "";
+            OpenFileDialog csv_fd = new OpenFileDialog();
+            csv_fd.FileName = "default.csv";
+            csv_fd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            csv_fd.Filter = "Excelファイル(*.csv)|*.csv|すべてのファイル(*.*)|*.*";
+            csv_fd.FilterIndex = 2;
+            csv_fd.Title = "変換したいCSVファイルを選択してください。";
+            csv_fd.RestoreDirectory = true;
+            csv_fd.CheckFileExists = true;
+            csv_fd.CheckPathExists = true;
+
+            if (csv_fd.ShowDialog() == DialogResult.OK)
+            {
+                select_file_path=csv_fd.FileName;
+            }
+
+            this.textBox1.Text = select_file_path;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //変換を開始するボタン
+            MessageBox.Show("変換を開始します。");
+            string file_path = "";
+            string line = "";
+            string output_path = "";
+            file_path = this.textBox1.Text;
+            output_path = file_path;
+            output_path = output_path.Replace(".csv", "");
+            output_path = output_path.Replace(".CSV", "");
+            output_path = output_path + "出力.csv";
+            StreamWriter out_sr = new StreamWriter(output_path, false, Encoding.GetEncoding("Shift_JIS"));
+            StreamReader sr = new StreamReader(file_path, Encoding.GetEncoding("shift_jis"));
+            {
+                // 末尾まで繰り返す
+                while (sr.Peek() >= 0)
+                {
+                    // CSVファイルの一行を読み込む
+                    line = sr.ReadLine();
+                    Console.WriteLine(line);
+                    if (line.Substring(0, 1) == "1")
+                    {
+                        out_sr.Write('"' + line.Substring(0, 1) + '"' + ",");
+                        out_sr.Write('"' + line.Substring(1, 2) + '"' + ",");
+                        out_sr.Write('"' + line.Substring(3, 1) + '"' + ",");
+                        out_sr.Write('"' + line.Substring(4, 10) + '"' + ",");
+                        out_sr.Write('"' + line.Substring(14, 40) + '"' + ",");
+                        out_sr.Write('"' + line.Substring(54, 4) + '"' + ",");
+                        out_sr.Write('"' + " " + '"' + ",");
+                        out_sr.Write("\r\n");
+                    }
+                    else if (line.Substring(0, 1) == "2") {
+                        out_sr.Write('"' + line.Substring(0, 1) + '"' + ",");
+                        out_sr.Write('"' + line.Substring(1, 4) + '"' + ",");
+                        out_sr.Write('"' + line.Substring(5, 15) + '"' + ",");
+                        out_sr.Write('"' + line.Substring(20, 3) + '"' + ",");
+                        out_sr.Write('"' + line.Substring(23, 15) + '"' + ",");
+                        out_sr.Write('"' + " " + '"' + ",");
+                        out_sr.Write('"' + line.Substring(42, 1) + '"' + ",");
+                        out_sr.Write('"' + line.Substring(43, 7) + '"' + ",");
+                        out_sr.Write('"' + line.Substring(50, 30) + '"' + ",");
+                        out_sr.Write('"' + line.Substring(80, 10) + '"' + ",");
+                        out_sr.Write('"' + line.Substring(90, 1) + '"' + ",");
+                        out_sr.Write('"' + line.Substring(97, 14) + '"' + ",");
+                        out_sr.Write('"' + line.Substring(111, 1) + '"' + ",");
+                        out_sr.Write('"' + " " + '"' + ",");
+                        out_sr.Write("\r\n");
+                    }
+                    else if (line.Substring(0, 1) == "8")
+                    {
+                        out_sr.Write('"' + line.Substring(0, 1) + '"' + ",");
+                        out_sr.Write('"' + line.Substring(1, 6) + '"' + ",");
+                        out_sr.Write('"' + line.Substring(7, 12) + '"' + ",");
+                        out_sr.Write('"' + line.Substring(19, 6) + '"' + ",");
+                        out_sr.Write('"' + line.Substring(25, 12) + '"' + ",");
+                        out_sr.Write('"' + line.Substring(38, 6) + '"' + ",");
+                        out_sr.Write('"' + line.Substring(43, 12) + '"' + ",");
+                        out_sr.Write('"' + " " + '"' + ",");
+                        out_sr.Write("\r\n");
+                    }
+                    else if (line.Substring(0, 1) == "9")
+                    {
+                        out_sr.Write('"' + line.Substring(0, 1) + '"' + ",");
+                        out_sr.Write('"' + " " + '"');
+                        out_sr.Write("\r\n");
+                    }
+                }
+            }
+            out_sr.Write(" ");
+            sr.Close();
+            out_sr.Close();
+            MessageBox.Show("変換が完了しました。");
+        }
+    }
+}
